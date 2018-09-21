868f683a343a3ef005ddc308e19742802796ce77
ol4ash <shaukavets@gmail.com>
2018-09-22 00:33:21 +0300

    task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number) {
+
+var num = number;
+
+var zeros=0;
+var i = 1;
+
+while (parseInt(num/Math.pow(5,i))>0) {
+
+zeros = zeros + parseInt(num/Math.pow(5,i));
+i++;
+}
+
+  
+return zeros
+
+
+
   // your implementation
 }
